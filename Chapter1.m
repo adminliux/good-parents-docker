1a9a6975cac3604a4ab1e45dd9aa0408924bbcbe
Your Name <you@example.com>
2019-04-22 18:18:34 +0800

    a

---
+++ Chapter1.m
@@ -0,0 +1,11 @@
+#import <Foundation/Foundation.h>
+
+int main(int argv, char *argc[]){
+  @autoreleasepool{
+    int a,b;
+    a = 1;
+    b = 2;
+    NSLog(@"Hello word %i",a + b);
+  };
+  return 0;
+}
